5c46d417b93e153c43c25a09392a341dec9e8e05
rishflab <rishflab@hotmail.com>
2018-08-29 21:22:18 +1000

    basic inventory

--- cart/cart.go
+++ cart/cart.go
@@ -1,9 +1,16 @@
 package cart
 
-type Cart struct {
-	items map[string]Item
+import "github.com/rishflab/shopping_cart/inventory"
+
+type cart struct {
+	items map[string]inventory.Item
+	*inventory.Inventory
 }
 
-func (c *Cart) AddToCart(item Item) {
+func (c *cart) AddItem(item inventory.Item) {
+
+}
+
+func (c *cart) RemoveItem(item inventory.Item) {
 
 }
--- inventory/inventory.go
+++ inventory/inventory.go
@@ -1,10 +1,37 @@
 package inventory
 
-type Inventory struct {
-	items map[string]Item
+import "fmt"
+
+type inventory struct {
+	quantity map[string]uint
+	price    map[string]float64
 }
 
-type Item struct {
-	quantity uint
-	price    float64
+func (inv *inventory) setPriceAndQuantity(name string, price float64, quantity uint) {
+	inv.quantity[name] = quantity
+	inv.price[name] = price
+}
+
+func (inv *inventory) getQuantity(name string) (uint, error) {
+	quantity, some := inv.quantity[name]
+	if some == true {
+		return quantity, nil
+	}
+	return 0, &itemNotFoundError{name}
+}
+
+func (inv *inventory) getPrice(name string) (float64, error) {
+	price, some := inv.price[name]
+	if some == true {
+		return price, nil
+	}
+	return -1.0, &itemNotFoundError{name}
+}
+
+type itemNotFoundError struct {
+	itemName string
+}
+
+func (e *itemNotFoundError) Error() string {
+	return fmt.Sprintf("%s not found in inventory", e.itemName)
 }
